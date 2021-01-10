bc2684433e710f51c4f53c36a71eee84c6eeffae
Stefan <stefan@yo-code.de>
2021-01-10 15:48:28 +0100

    remove unused js and css asset integrations

--- lib/generators/spree_dynamic_variants/install/install_generator.rb
+++ lib/generators/spree_dynamic_variants/install/install_generator.rb
@@ -4,14 +4,6 @@ module SpreeDynamicVariants
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_dynamic_variants\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_dynamic_variants\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_dynamic_variants'
       end
